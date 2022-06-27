2d7a5ec9efc8a2b379def9c92691c7ec865e050a
JinSeop Kim <93374348+uskhbs@users.noreply.github.com>
2022-06-27 18:29:23 +0900

    [Silver IV] Title: 듣보잡, Time: 100 ms, Memory: 43584 KB -BaekjoonHub

--- 백준/Silver/1764. 듣보잡/듣보잡.py
+++ 백준/Silver/1764. 듣보잡/듣보잡.py
@@ -2,15 +2,11 @@ import sys
 
 n,m = map(int,sys.stdin.readline().split())
 
-dh = []
-for _ in range(n) :
-    dh.append(sys.stdin.readline().strip())
+data = sys.stdin.read().splitlines()
+dh = set(data[:n])
+ds = set(data[n:])
 
-ds = []
-for _ in range(m) :
-    ds.append(sys.stdin.readline().strip())
-
-result = list(sorted((set(dh) & set(ds))))
+result = sorted(list(dh&ds))
 print(len(result))
 for i in result :
     print(i)
