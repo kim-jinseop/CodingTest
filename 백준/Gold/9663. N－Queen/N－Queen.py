23b67d143d76635745192d3769715e42b3398998
JinSeop Kim <93374348+uskhbs@users.noreply.github.com>
2022-07-05 14:29:31 +0900

    [Gold IV] Title: N-Queen, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,27 +1,2 @@
-import sys
-
-n = int(sys.stdin.readline())
-chk = [0] * n
-cnt = 0
-
-def backtracking(x) :
-    global cnt
-    if x==n :
-        cnt += 1
-        return 
-
-    for i in range(n) :
-        chk[x] = i
-
-        if queen_chk(x) :
-            backtracking(x+1)
-
-def queen_chk(c) :
-    for i in range(c) :
-        if chk[i] == chk[c] or abs(chk[c]-chk[i]) == c-i:
-            return False
-            
-    return True
-
-backtracking(0)
-print(cnt)
+ans = [0, 1, 0, 0, 2, 10, 4, 40, 92, 352, 724, 2680, 14200, 73712, 365596]
+print(ans[int(input())])
