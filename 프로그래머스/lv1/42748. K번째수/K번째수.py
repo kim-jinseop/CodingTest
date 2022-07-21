993806b4e54e94badfc7b59efcac449c4cab784c
JinSeop Kim <93374348+uskhbs@users.noreply.github.com>
2022-07-21 23:08:56 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42748. K번째수/K번째수.py
+++ 프로그래머스/lv1/42748. K번째수/K번째수.py
@@ -1,13 +1,9 @@
-import copy
-
 def solution(array, commands):
     result = []
     for part in commands :
         i,j,k = part
         
-        arr = array[i-1:j]
-        arr.sort()
-        
+        arr = sorted(array[i-1:j])
         result.append(arr[k-1])
 
     return result
