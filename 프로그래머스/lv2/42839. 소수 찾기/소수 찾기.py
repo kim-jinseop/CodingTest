b8c36ff62c6c1d56b65e4b2608c373edbf03219c
JinSeop Kim <kimjinseop5697@gmail.com>
2022-08-03 18:30:10 +0900

    [level 2] Title: 소수 찾기, Time: 8.72 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,30 +1,20 @@
 from itertools import permutations
 def solution(numbers):
     answer = 0
-    box = []
-    calbox = []
+    box = set()
+    for i in range(len(numbers)) :
+        box |= set(map(int,map(''.join,permutations(numbers,i+1))))
     
-    for i in range(1,len(numbers)+1) :
-        box.extend(permutations(numbers,i))
-        
-    for i in box[:] :
-        if i[0] == '0' :
-            box.remove(i)
-        else :
-            calbox.append(int(''.join(i)))
-    
-    calbox = set(calbox)
-    Max = max(calbox)
+    box -= set(range(0,2))
+    Max = max(box)
     
     data = [0] * (Max+1)
-    data[0] = 1
-    data[1] = 1
     for i in range(2, Max+1):
         if data[i] == 0 :
             for j in range(i*2,Max+1,i) :
                 data[j] = 1
     
-    for part in calbox :
+    for part in box :
         if data[part] == 0 :
             answer += 1
             
