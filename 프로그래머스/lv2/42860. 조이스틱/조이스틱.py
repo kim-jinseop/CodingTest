33475abe7d8edfe2be704db889c3f45badc09dcd
JinSeop Kim <kimjinseop5697@gmail.com>
2022-08-10 13:38:50 +0900

    [level 2] Title: 조이스틱, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
+++ 프로그래머스/lv2/42860. 조이스틱/조이스틱.py
@@ -1,21 +1,19 @@
-def solution(name):
-    answer = len(name)-1  # 좌우로 움직임의 최대값(A가 없을 경우)
-    
-    #좌우 움직임의 최솟값
-    i = 1
-    while i < len(name):
-        if name[i] == 'A':
-            R = i-1
-            i += 1
-            while i < len(name) and name[i] == 'A':
-                i += 1
-            L = len(name)-i
-            answer = min([answer, 2*L + R, 2*R + L])
-        else:
-            i += 1
-
+def solution(name) :
+    answer = 0
+    Min = len(name)
+    #상하
     for i in name :
-        if i != 'A' :
+        if i != "A" :
             answer += min((ord('Z')-ord(i)+1),(ord(i)-ord('A')))
-
+    
+    leng = len(name)
+    #좌우
+    for i in range(leng):
+        index = i + 1
+        while index<leng and name[index] == 'A' :
+            index += 1
+            
+        Min = min(Min, i*2+leng-index,2*(leng-index)+i)
+        
+    answer += Min
     return answer
