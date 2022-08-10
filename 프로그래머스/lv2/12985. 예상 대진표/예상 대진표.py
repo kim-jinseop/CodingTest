a74f3c58632b733d9e7341b0c5529fa326eba9ba
JinSeop Kim <kimjinseop5697@gmail.com>
2022-08-10 22:37:10 +0900

    [level 2] Title: 예상 대진표, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,14 @@
+def solution(n,a,b):
+    answer = 0
+
+    while a!=b :
+        if a%2!=0 :
+            a += 1
+        if b%2!=0 :
+            b += 1
+            
+        answer += 1
+        a //= 2
+        b //= 2
+        
+    return answer
