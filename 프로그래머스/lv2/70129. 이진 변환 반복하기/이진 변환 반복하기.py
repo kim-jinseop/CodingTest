62e2a194444bdd79d4b55b1b335aa191625971a5
JinSeop Kim <kimjinseop5697@gmail.com>
2022-08-15 19:14:39 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 0.20 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
+++ 프로그래머스/lv2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -1,18 +1,11 @@
 def solution(s):
-    answer = []
     cnt_zero = 0
     cnt_change = 0
     
     while s!='1' :
-        b_leng = len(s)
-        s = s.replace('0','')
-        a_leng = len(s)
-        
-        cnt_zero += b_leng-a_leng
-        
-        s = bin(a_leng)[2:]
+        num = s.count('1')        
+        cnt_zero += len(s) - num       
+        s = bin(num)[2:]
         cnt_change += 1
     
-    answer.append(cnt_change)
-    answer.append(cnt_zero)
-    return answer
+    return [cnt_change,cnt_zero]
