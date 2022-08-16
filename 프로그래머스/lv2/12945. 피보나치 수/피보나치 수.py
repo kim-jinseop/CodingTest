a46e59e83f1b35d279767b8711ebcb22d13d3125
JinSeop Kim <kimjinseop5697@gmail.com>
2022-08-17 01:16:32 +0900

    [level 2] Title: 피보나치 수, Time: 6.75 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.py
@@ -1,7 +1,14 @@
-def solution(n):
-    box = [0] * (n+1)
-    box[1] = 1
+# def solution(n):
+#     box = [0] * (n+1)
+#     box[1] = 1
     
-    for i in range(2,n+1) :
-        box[i] = (box[i-1] + box[i-2]) %1234567
-    return box[n]
+#     for i in range(2,n+1) :
+#         box[i] = (box[i-1] + box[i-2]) %1234567
+#     return box[n]
+
+
+def solution(n):
+    a,b = 0,1
+    for i in range(n):
+        a,b = b,(a+b)%1234567
+    return a
