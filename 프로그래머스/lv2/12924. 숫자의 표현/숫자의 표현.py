9765bb338da960e9654d673ff7f33ededbb730c4
JinSeop Kim <kimjinseop5697@gmail.com>
2022-08-18 00:41:23 +0900

    [level 2] Title: 숫자의 표현, Time: 2.85 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,20 +1,33 @@
+# def solution(n):
+#     answer = 0
+#     arr = list(range(1,n+1))
+    
+#     l = 0
+#     r = 0
+#     save = 0
+#     while r<n :
+#         if save < n :
+#             save += arr[r]
+#             r += 1
+#         elif save == n :
+#             answer +=1
+#             save -= arr[l]
+#             l += 1
+#         else :
+#             save -= arr[l]
+#             l += 1
+            
+#     return answer+1
+
+
 def solution(n):
     answer = 0
-    arr = list(range(1,n+1))
-    
-    l = 0
-    r = 0
-    save = 0
-    while r<n :
-        if save < n :
-            save += arr[r]
-            r += 1
-        elif save == n :
-            answer +=1
-            save -= arr[l]
-            l += 1
-        else :
-            save -= arr[l]
-            l += 1
+    for i in range(1, n + 1):
+        s = 0
+        while s < n:
+            s += i
+            i += 1
+        if s == n:
+            answer += 1
             
-    return answer+1
+    return answer
