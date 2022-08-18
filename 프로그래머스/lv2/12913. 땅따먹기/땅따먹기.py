30c390703e4cb1b01cb939ab4dbacc426dc3bdf6
JinSeop Kim <kimjinseop5697@gmail.com>
2022-08-19 01:27:02 +0900

    [level 2] Title: 땅따먹기, Time: 194.54 ms, Memory: 32.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
+++ 프로그래머스/lv2/12913. 땅따먹기/땅따먹기.py
@@ -1,17 +1,8 @@
-import copy 
 def solution(land):
     answer = 0
     
-    for idx in range(len(land)-1) :
-        #case1
-        mi = land[idx].index(max(land[idx]))
-        x = land[idx].pop(mi) # 1st
-        y = land[idx].pop(land[idx].index(max(land[idx]))) # 2en
-        
-        for i in range(4) :
-            if i == mi : 
-                land[idx+1][i] += y
-            else :
-                land[idx+1][i] += x
-    
+    for i in range(1,len(land)) :
+        for j in range(len(land[0])) :
+            land[i][j] += max(land[i-1][:j] + land[i-1][j+1:]) 
+
     return max(land[len(land)-1])
