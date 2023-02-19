a6958b1d5e45c6a70c80fb1d74305971baa6110f
JinSeop Kim <kimjinseop5697@gmail.com>
2023-02-19 19:52:20 +0900

    [unrated] Title: 유사 칸토어 비트열, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
@@ -0,0 +1,29 @@
+# 1 => 5^0 => 1 => 1개
+# 2 => 5^1 => 11011 => 1의 개수 4개
+# 3 => 5^2 => 11011 11011 00000 11011 11011 => 1의 개수 4^2
+# 4 => 5^3 => 16/16/0/16/16 -> 1의 개수 4^3
+
+bit = '11011'
+def solution(n, l, r):
+    def func(x) :
+        if x <= 5 : 
+            return bit[:x].count('1')
+    
+        cnt = 1 
+        while 5**(cnt+1) < x :
+            cnt += 1 
+            
+        share, remainder = divmod(x, 5**cnt)
+        
+        answer = share * (4**cnt)
+        
+        if share >= 3 :
+            answer -= 4**cnt
+            
+        if share == 2:
+            return answer
+        else:
+            return answer + func(remainder)
+
+    return func(r) - func(l-1) 
+
