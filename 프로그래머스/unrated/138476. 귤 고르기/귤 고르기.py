d7138ee4c279257717fb74675344d0b30773adc4
JinSeop Kim <kimjinseop5697@gmail.com>
2023-03-05 22:26:41 +0900

    [unrated] Title: 귤 고르기, Time: 28.50 ms, Memory: 21.1 MB -BaekjoonHub

--- 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.py
@@ -1,17 +1,11 @@
-from collections import defaultdict
+from collections import Counter
 
 def solution(k, tangerine):
     answer = 0
-    d = defaultdict(int)
     
-    for i in tangerine :
-        d[i] += 1
-    
-    d = sorted(d.items(),key=lambda item: item[1], reverse=True)
-
-    for _, i in d :
-        answer += 1 
-        k -= i
-        if k<=0 :
+    for _,value in Counter(tangerine).most_common() :
+        answer += 1
+        k -= value
+        if k <= 0 :
             break
     return answer
