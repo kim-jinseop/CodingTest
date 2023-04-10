489ac8a17e60a9ff06fba3562d5f8e5f7d7765dd
JinSeop Kim <kimjinseop5697@gmail.com>
2023-04-11 03:50:48 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 144 ms, Memory: 36280 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,28 @@
+from sys import stdin
+from collections import deque
+
+n, k = map(int,stdin.readline().split())
+arr = [-1] * 100001
+q = deque()
+q.append((0,n))
+
+while q :
+    time, target = q.popleft()
+
+    if arr[target] != -1 :
+        continue
+
+    arr[target] = time
+
+    if arr[k] != -1 :
+        print(arr[k])
+        break
+
+    if target > 0 :
+        q.append((time+1, target-1))
+
+    if target < 100000 :
+        q.append((time+1, target+1))  
+    
+    if target*2 < 100001 :
+        q.appendleft((time, target*2))
